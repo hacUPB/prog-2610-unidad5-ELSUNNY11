e093c8005dd1360f1692f8eaee6c24db0b2a16eb
Santiago Marin <santiago.marinb@upb.edu.co>
2026-04-22 15:42:52 -0500

    actividad 1

--- archivos/ejercicio_2.py
+++ archivos/ejercicio_2.py
@@ -1,5 +1,7 @@
 fp = open(".\\archivos\\ejemplo_2.txt", encoding="utf-8") #encoding="utf-8" sirve para que funcionen los acentos en la lengua ESPAÑOLA DEL REINO DE CASTILLA Y LEÓN
-datos_1 = fp.readlines()
-print("Primera lectura:", datos_1)
+lista = fp.readlines()
+fp.close
+print(lista)
 
-datos_2 = fp.read[]
+for linea in lista:
+    print(linea[0])
